97470e96010ba982f8c9692d31e02a63abe138cc
Tom Shorrock <t.h.shorrock@gmail.com>
2011-02-28 22:29:40 +0000

    added test programs

---
+++ test_programs/analogic_with_coms_manager/src/analogic_test_1.cpp
@@ -0,0 +1,21 @@
+#include "coms_manager.hpp"
+int
+main  (int ac, char **av)
+{
+  //use the coms namespace
+  using namespace ICR::coms;
+  
+  
+  //Open a serial connection to the analogic device attached to "/dev/ttyUSB0" port 
+  serial_manager serial("/dev/ttyUSB0",
+			  9600,
+			  flow_control::hardware
+			  ); 
+  
+  
+  
+  //send a command to the serial device
+  serial.send("RUN\r");
+
+  //The connections automatically close when out of scope.
+}
