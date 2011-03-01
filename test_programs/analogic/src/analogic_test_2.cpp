4a42b53ac181699c8d5e6bb238cf6496b07f7343
Tom Shorrock <t.h.shorrock@gmail.com>
2011-03-01 15:48:35 +0000

    working examples

--- test_programs/analogic/src/analogic_test_2.cpp
+++ test_programs/analogic/src/analogic_test_2.cpp
@@ -8,10 +8,10 @@ main  (int ac, char **av)
   
   //Open a serial connection to a device attached to "COM1" port
   analogic_remote_control analogic("/dev/ttyUSB0");
-  
-  analogic.clock_rate(1.25e-9);  //set fastest clock speed
-  analogic.expression("AT TRIG FOR 28u 0.0 FOR 2u 0.5 FOR 1u 0");  //create a pulse
-  analogic.turn_on();  //run
+  analogic.echo(true);
+  analogic.expression("AT TRIG RPT 1 ( FOR 30u 0.0 FOR 2u 0.5 FOR 1u 0) CLK = 1.25n MARK = 20u");  //create a pulse
+  //analogic.turn_on();  //run
+  //analogic.turn_off();  
 
 
   //The connections automatically close when out of scope.
