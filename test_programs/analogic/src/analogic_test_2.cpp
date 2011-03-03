9fca2c859df56e925bc3dea923490a6b7968722f
Tom Shorrock <t.h.shorrock@gmail.com>
2011-03-03 20:47:53 +0000

    trying to get data transfer with anologic working - it still does not

--- test_programs/analogic/src/analogic_test_2.cpp
+++ test_programs/analogic/src/analogic_test_2.cpp
@@ -1,18 +1,49 @@
 #include "analogic.hpp"
+#include "stringify.hpp"
+
+
 int
 main  (int ac, char **av)
 {
   //use the coms namespace
   using namespace ICR::analogic;
+  using namespace ICR;
   
   
-  //Open a serial connection to a device attached to "COM1" port
+  // //Open a serial connection to a device attached to "COM1" port
   analogic_remote_control analogic("/dev/ttyUSB0");
   analogic.echo(true);
-  analogic.expression("AT TRIG RPT 1 ( FOR 30u 0.0 FOR 2u 0.5 FOR 1u 0) CLK = 1.25n MARK = 20u");  //create a pulse
-  //analogic.turn_on();  //run
-  //analogic.turn_off();  
+  for(size_t i=0;i<20;++i){
 
+
+    analogic.expression("AT TRIG RPT 1 ( FOR "+stringify(i)+"u 0.0 FOR 2u 0.5 FOR 1u 0) CLK = 1.25n MARK = 20u");  //create a pulse
+    analogic.turn_on();  
+
+  
+  }
+  //run
+  // analogic.turn_off();  
+
+  //make some data
+  // constant_segment c1(32e-6, 0.1);
+  // constant_segment c2(2e-6, 0.3);
+  // constant_segment c3(16e-6, 0.0);
+  
+  // segment_container s;
+  // s.add(c1);
+  // s.add(c2);
+  // s.add(c3);
+  
+  // analogic_data data(1.25e-9);
+  // data.add(&c1);
+  // //data.add(&c2);
+  // //data.add(&c3);
+  
+  // analogic.data(data);
+
+  
+    
+    
 
   //The connections automatically close when out of scope.
 }
